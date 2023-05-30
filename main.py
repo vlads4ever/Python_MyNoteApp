dd7e6fd2c6f9ee044f9ad19e8eebac67d4667e0a
vladimir_skrypnikov <sevgrad@list.ru>
2023-05-30 21:06:59 +0300

    Serializing added

--- main.py
+++ main.py
@@ -4,23 +4,21 @@ from notes.list_note import ListNote
 from notes.text_note import TextNote
 
 if __name__ == '__main__':
-    text_note = TextNote('Текстовая заметка №1', 'Первая заметка посвящена началу работы над проектом MyNoteApp.')
-    text_note_2 = TextNote('Текстовая заметка №2', 'Вторая заметка посвящена продолжению работы над проектом MyNoteApp.')
+    # text_note = TextNote('Текстовая заметка №1', 'Первая заметка посвящена началу работы над проектом MyNoteApp.')
+    # text_note_2 = TextNote('Текстовая заметка №2', 'Вторая заметка посвящена продолжению работы над проектом MyNoteApp.')
+    #
+    # shoping_list = ["Молоко", "Мясо", "Сыр"]
+    # list_note = ListNote('Список покупок №1', shoping_list)
+    #
+    # notes = NotesStorage()
+    # notes.push_to_storage(text_note)
+    # notes.push_to_storage(text_note_2)
+    # notes.push_to_storage(list_note)
 
-    shoping_list = ["Молоко", "Мясо", "Сыр"]
-    list_note = ListNote('Список покупок №1', shoping_list)
 
 
-    notes = NotesStorage()
-    notes.push_to_storage(text_note)
-    notes.push_to_storage(text_note_2)
-    notes.push_to_storage(list_note)
-    print(notes.get_str_list())
-    print()
 
-    j_saved = JSONDataAdapter.to_json(list_note)
-    print(j_saved)
-    list_note_restored = JSONDataAdapter.from_json(j_saved)
-    print(list_note_restored)
-
+    # JSONDataAdapter.to_json(notes)
+    new_notes = JSONDataAdapter.from_json("NyNotes.json")
+    print(new_notes.get_str_list())
 
--- saving/json_data_adapter.py
+++ saving/json_data_adapter.py
@@ -1,28 +1,30 @@
 import json
 
 from notes.list_note import ListNote
+from notes.text_note import TextNote
+from storage.notes_storage import NotesStorage
 
 
 class JSONDataAdapter:
     @staticmethod
     def to_json(o):
-        if isinstance(o, ListNote):
-            return json.dumps({
-                "id_note": o.id_note,
-                "header_note": o.header_note,
-                "body_note": o.body_note,
-                "creation_time": o.creation_time
-            }, indent=5, sort_keys=True)
+        with open("NyNotes.json", "w") as fw:
+            if isinstance(o, NotesStorage):
+                json.dump([o.__dict__ for o in o.list_notes], fw, indent=5, sort_keys=True)
 
     @staticmethod
-    def from_json(o):
-        o = json.loads(o)
-
-        print(type(o.header_note))
-        print(type(o.body_note))
-
-        try:
-            list_note = ListNote(o.header_note, o.body_note)
-            return list_note
-        except AttributeError:
-            print("Неверная структура")
+    def from_json(fw):
+        with open(fw, "r") as fr:
+            o = json.load(fr)
+            print(type(o))
+            try:
+                list_notes = NotesStorage()
+                for item in o:
+                    if item['id_note'][0] == 'l':
+                        note = ListNote(item['header_note'], item['body_note'])
+                    elif item['id_note'][0] == 't':
+                        note = TextNote(item['header_note'], item['body_note'])
+                    list_notes.push_to_storage(note)
+                return list_notes
+            except AttributeError:
+                print("Неверная структура")
